0eaf025d758cdcd9aba9194427ebea8d61b49d6e
macrobian88 <ramshankarsc9@gmail.com>
2025-11-11 11:07:35 +0530

    Add config files for desktop and documentation

---
+++ hr_suite/config/desktop.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "HR Suite",
+            "color": "#2ecc71",
+            "icon": "octicon octicon-organization",
+            "type": "module",
+            "label": _("HR Suite"),
+            "description": _("Complete HR Management Solution"),
+        }
+    ]
---
+++ hr_suite/config/docs.py
@@ -0,0 +1,7 @@
+source_link = "https://github.com/yourusername/hr_suite"
+docs_base_url = "https://docs.yourcompany.com"
+headline = "Complete HR Management Solution"
+sub_heading = "One-click setup for ERPNext HR Module"
+
+def get_context(context):
+    context.brand_html = "HR Suite"
