ff30fa6d448f619dcfd145fe92628c17df0b3598
macrobian88 <ramshankarsc9@gmail.com>
2025-11-11 11:03:46 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
+from hr_suite import __version__ as version
+
+setup(
+    name="hr_suite",
+    version=version,
+    description="Complete HR Management Solution - One Click Setup",
+    author="Your Company",
+    author_email="support@yourcompany.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
+)
