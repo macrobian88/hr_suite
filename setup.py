8b22f522dc369f7c5c551b14b7def95298a45128
macrobian88 <ramshankarsc9@gmail.com>
2025-11-11 11:26:04 +0530

    Add employee portal, setup.py, requirements, and license

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
+# get version from __version__ variable in hr_suite/__init__.py
 from hr_suite import __version__ as version
 
 setup(
     name="hr_suite",
     version=version,
-    description="Complete HR Management Solution - One Click Setup",
+    description="Complete HR Management Solution with ERPNext",
     author="Your Company",
     author_email="support@yourcompany.com",
     packages=find_packages(),
